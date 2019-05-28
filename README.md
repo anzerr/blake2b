9498aacc0776aee9952cb835466502ab1b4f114f
anzerr <ryan.anzer@epitech.eu>
2019-05-28 11:36:53 +0200

    dump

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Blake2B in Javascript (cleaned up version)
 
 #### `Install`
 ```shell
-npm install --save git+http://git@github.com/anzerr/blake2b.git
+npm install --save git+https://git@github.com/anzerr/blake2b.git
 ```
 
 #### `Example`
@@ -14,5 +14,5 @@ const blake = require('blake2b');
 let context = blake.createHash({digestLength: 8});
 context.update('cat');
 context.update('dog');
-console.log(context.digest()); // 3346a0d3f9b3a626
+console.log(context.digest().toString('hex')); // 3346a0d3f9b3a626
 ```
--- example.js
+++ example.js
@@ -9,3 +9,20 @@ context.update('dog');
 assert.equal(context.digest().toString('hex'), hex);
 assert.equal(blake.hex('catdog', 8), hex);
 assert.equal(blake.hash('catdog', 8).toString('hex'), hex);
+
+(() => {
+	let hash = () => {
+		let c = blake.createHash({digestLength: 8});
+		c.update('cat');
+		return c.digest().toString('hex');
+	};
+
+	const NS_PER_SEC = 1e9, rounds = 100000;
+	let start = process.hrtime();
+	for (let i = 0; i < rounds; i++) {
+		hash();
+	}
+	const diff = process.hrtime(start),
+		hps = NS_PER_SEC / ((diff[0] * NS_PER_SEC + diff[1]) / rounds);
+	console.log(`Benchmark ${hps.toFixed(2)}/ hashes per second`);
+})();
